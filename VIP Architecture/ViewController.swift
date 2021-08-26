9a29eed010b1c801ea814b18f7586ed85ba5e09d
Waldison Junior <waldison.junior@picpay.com>
2021-08-26 13:49:37 -0300

    Initial Commit

---
+++ VIP Architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIP Architecture
+//
+//  Created by Waldison Junior on 26/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
